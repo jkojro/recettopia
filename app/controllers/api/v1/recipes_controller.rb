11575deb0d2e7b1aaf82ee79bdf42b9bcf4786e7
Janusz Kojro <janusz@ragnarson.com>
2024-03-02 22:48:27 +0100

    Add jbuilder views

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RecipesController < ActionController::API
   def search
+    @recipes = [Recipe.create(title: "Test")]
   end
 end
---
+++ app/views/api/v1/recipes/_recipe.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! recipe, :id, :title, :vegan, :gluten_free, :instructions
